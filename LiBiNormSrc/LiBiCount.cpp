c3cdc33de25c33064fc930067d4405b4212a380c
Nigel Dyer <N.P.Dyer@warwick.ac.uk>
2016-07-16 08:08:54 +0000

    Extensions to gtf file parser.   Need to chack backwards compatibility
    git-svn-id: svn+ssh://cyverse.warwick.ac.uk/home/svn/TRegS_software/LiBiNorm/trunk@2097 d27ce7f2-3f32-4a89-82b7-fbde2a400052

--- LiBiNormSrc/LiBiCount.cpp
+++ LiBiNormSrc/LiBiCount.cpp
@@ -3,15 +3,67 @@
 #include <crtdbg.h>
 #include "libCommon.h"
 #include "containerEx.h"
-#include "gtfFile.h"
 #include "api/BamReader.h"
 
 using namespace std;
 using namespace BamTools;
 
+void gtfFileEx::index(const string & feature,const string & attribute)
+{
+	size_t attribute_id = gtfEntryTags::idMap[attribute];
+
+	for (auto & chrom : entryMap)
+	{
+		chromosomeData & thisChromData = chromData[chrom.first];
+		for (auto entry : chrom.second)
+		{
+			if (entries[entry].type == feature)
+			{
+				string attName;
+				for (auto & tag : entries[entry].tags)
+				{
+					if (tag.type == attribute_id)
+					{
+						attName = tag.val;
+						break;
+					}
+				}
+				chromosomeData::iterator i = thisChromData.find(entries[entry].start);
+				bool duplicate = false;
+
+				while (i != thisChromData.end() && (i->first == entries[entry].start))
+				{
+					if ((i->second.finish == entries[entry].finish) &&
+						(i->second.name == attName))
+					{
+						duplicate = true;
+						break;
+					}
+					i++;
+				}
+				if (!duplicate)
+				{
+					thisChromData.emplace(entries[entry].start,region(entries[entry].finish,attName));
+				}
+			}
+		}
+		//	And now produce overlap list
+		for (chromosomeData::iterator i = thisChromData.begin(); i != thisChromData.end();i++)
+		{
+			for (chromosomeData::iterator j = i;(j != thisChromData.end()) && (j->first < i->second.finish);j++)
+				j->second.overlaps.push_back(&i->second);
+		}
+	}
+}
+
+
+
+
 int LiBiCount::main(int argc, char **argv)
 {
-	string bamFileName,gtfFileName;
+	string bamFileName,gtfFileName,
+		feature = "exon",
+		attribute = "gene_id";
 
 	if(argc < 1)
 	{
@@ -52,12 +104,16 @@ int LiBiCount::main(int argc, char **argv)
 	const RefVector references = reader.GetReferenceData();
 */
 
-	gtfFile genomeDef;
+	gtfFileEx genomeDef;
 
 	genomeDef.open(gtfFileName);
+	cout << "Data read";
 
+	genomeDef.index(feature,attribute);
 
-
+	string test;
+	cout << "Finished";
+	cin >> test;
 
 
 	return EXIT_SUCCESS;
--- LiBiNormSrc/LiBiCount.h
+++ LiBiNormSrc/LiBiCount.h
@@ -1,6 +1,39 @@
 #ifndef LIBICOUNT_H
 #define LIBICOUNT_H
 
+#include "gtfFile.h"
+
+using namespace std;
+
+class region
+{
+public:
+	size_t finish;
+	vector<region *> overlaps;
+	string name;
+	region(	size_t finish,const string name):finish(finish),name(name){};
+
+};
+
+
+class chromosomeData : public multimap<size_t, region> 
+{
+
+};
+
+
+class gtfFileEx : public gtfFile
+{
+	map<string,chromosomeData> chromData; 
+
+public: 
+	void index(const string & feature,const string & attribute);
+
+
+
+};
+
+
 class LiBiCount
 {
 
