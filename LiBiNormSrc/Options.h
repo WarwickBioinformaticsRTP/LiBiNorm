5a62a88ee8da86ab328995b0199a843bbfbb5015
Nigel Dyer <N.P.Dyer@warwick.ac.uk>
2017-01-19 10:43:33 +0000

    Check for nelder mead asymptote
    git-svn-id: svn+ssh://cyverse.warwick.ac.uk/home/svn/TRegS_software/LiBiNorm/trunk@2369 d27ce7f2-3f32-4a89-82b7-fbde2a400052

--- LiBiNormSrc/Options.h
+++ LiBiNormSrc/Options.h
@@ -15,7 +15,7 @@
 
 //	Nelder Mead
 #define USE_NELDER_MEAD_FOR_INITIAL_VALUES
-#define NELDER_MEAD_ITERATIONS 20
+#define NELDER_MEAD_ITERATIONS 2000
 #define EDGE_PENALTY_MULTIPLIER 1000
 #define PARAMETER_WEIGHTING_SLOPE 2
 
@@ -61,7 +61,7 @@
 //	Use this option to use the parameters associated with the most likly parameter set
 //	https://sciencehouse.wordpress.com/2010/06/23/mcmc-and-fitting-models-to-data/
 //	rather than the median values
-#define USE_PARAMS_FROM_LOWEST_LL
+// #define USE_PARAMS_FROM_LOWEST_LL
 
 
 #endif
